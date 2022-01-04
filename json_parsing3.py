3d2009ba8b2ef84b703a6d771fba90ae5db63393
mossymoon <96680106+mossymoon@users.noreply.github.com>
2022-01-04 19:13:20 +0300

    Update json_parsing3.py

--- json_parsing3.py
+++ json_parsing3.py
@@ -1,5 +1,3 @@
-##дом задание №5 - не получается отфильтровать по выводу только с 2 message, отправил вопрос в чат
-
 import json
 
 second_message = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
