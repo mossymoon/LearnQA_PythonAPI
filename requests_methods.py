d5f176cb8e82681cbb18f2ee928b7a2481423afe
mossymoon <stephan.sergienko@gmail.com>
2022-01-05 22:04:44 +0300

    token_task

--- requests_methods.py
+++ requests_methods.py
@@ -1,18 +1,20 @@
 import requests
 
-payload_get = {"user": "name"}
-response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=payload_get)
+response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type")
 print(response.text)
 
-payload_post = {"user1": "name1"}
-response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payload_post)
+response = requests.options("https://playground.learnqa.ru/ajax/api/compare_query_type")
 print(response.text)
 
-payload_put = {"user2": "name2"}
-response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payload_put)
+response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data = {'method': 'POST'})
 print(response.text)
 
-payload_delete = {"user3": "name3"}
-response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payload_delete)
+response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data = {'method': 'GET'})
 print(response.text)
+
+
+
+
+
+
 
