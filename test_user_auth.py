8d1c617eb46ca5cf331a896674f3d4e10c066bb0
mossymoon <@stepanobuelo>
2024-06-22 09:21:37 +0300

    added test first api2

--- test_user_auth.py
+++ test_user_auth.py
@@ -7,19 +7,20 @@ class TestUserAuth:
             'password':'1234'
         }
 
-        response1 = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=data)
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
 
+        print(response1.cookies)
         assert "auth_sid" in response1.cookies, "There is no auth cookie in the response"
         assert "x-csrf-token" in response1.headers, "There is no CSRF token header in the response"
         assert "user_id" in response1.json(), "There is no user id in the response"
 
-        auth_sid = response1.cookies.get("auth_sid")
-        token = response1.headers.get("x_csrf-token")
+        auth_sid = response1.cookies["auth_sid"]
+        token = response1.headers["x-csrf-token"]
         user_id_from_auth_method = response1.json()["user_id"]
 
         response2 = requests.get(
-            "https://playground.learnqa.ru/api/check_auth_cookie",
-            headers={"x_csrf-token": token},
+            "https://playground.learnqa.ru/api/user/auth",
+            headers={"x-csrf-token": token},
             cookies={"auth_sid": auth_sid}
         )
 
